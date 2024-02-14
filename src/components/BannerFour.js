b051b7c89ca537b25ac4e123539e9109c54762ec
Akasharyan47 <akasharyan.aryan@gamil.com>
2024-02-15 04:08:34 +0530

    commit

--- src/components/BannerFour.js
+++ src/components/BannerFour.js
@@ -1,19 +1,46 @@
 import React from "react";
 import { FaPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { Slide } from "react-slideshow-image";
+import 'react-slideshow-image/dist/styles.css';
+
 
 const BannerFour = () => {
+  const bannerImages = [
+    {
+      url: require("../Image/bannerImage/1.jpg"),
+    },
+    {
+      url: require("../Image/bannerImage/2.jpg"),
+    },
+    {
+      url: require("../Image/bannerImage/1.jpg"),
+    },
+    {
+      url: require("../Image/bannerImage/1.jpg"),
+    },
+   
+     
+  ];
   return (
-    <> 
+    <>  
       <div className='banner-area bg-relative banner-area-3'>
 
         <div className='container-fluid pt-5 '>
-          <div className='bg-gray'>
-            <img
+          <div className='bg-gray   '>
+           <img
               className='img-right img-fluid'
               src='assets/img/banner-4/Preeti_jog.jpg'
               alt='img'
-            />
+            /> 
+            
+                  {/* <Slide>
+          {bannerImages.map((image, index) => (
+            <div key={index} className='each-slide'>
+              <img src={image.url} alt={`Slide ${index + 1}`} />
+            </div>
+          ))}
+        </Slide> */}
             <div className='container'>
               <div className='row'>
                 <div className='col-lg-7 align-self-center'>
@@ -49,6 +76,7 @@ const BannerFour = () => {
             </div>
           </div>
         </div>
+        
       </div>
 
       {/* ================== BannerFour End ==================*/}
